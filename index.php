d891ae450ef1775ade604123595832ec96c4f3c7
David Meister <thedavidmeister@gmail.com>
2013-02-04 01:03:12 +1100

    Converting whitespace to spaces
    Signed-off-by: David Meister <thedavidmeister@gmail.com>

--- index.php
+++ index.php
@@ -1,131 +1,131 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
-		<meta name="author" content="www.godeldesign.com.au" />
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
+    <meta name="author" content="www.godeldesign.com.au" />
 
-		<title>Compare a webpage agains up to three images --- Image A Trois</title>
+    <title>Compare a webpage agains up to three images --- Image A Trois</title>
 
-		<link type="text/css" media="all" rel="stylesheet" href="css/reset.css" />
-		<link type="text/css" media="all" rel="stylesheet" href="css/style.css" />
-		<link type="text/css" media="all" rel="stylesheet" href="css/layout.css" />
-		<link type="text/css" media="all" rel="stylesheet" href="lib/ia3.css" />
-		<script src="js/jquery.js" type="text/javascript" language="javascript"></script>
-		<script src="js/jquery.ui.js" type="text/javascript" language="javascript"></script>
-		<script src="lib/ia3.js" type="text/javascript" language="javascript"></script>
-		<script src="js/site.js" type="text/javascript" language="javascript"></script>
+    <link type="text/css" media="all" rel="stylesheet" href="css/reset.css" />
+    <link type="text/css" media="all" rel="stylesheet" href="css/style.css" />
+    <link type="text/css" media="all" rel="stylesheet" href="css/layout.css" />
+    <link type="text/css" media="all" rel="stylesheet" href="lib/ia3.css" />
+    <script src="js/jquery.js" type="text/javascript" language="javascript"></script>
+    <script src="js/jquery.ui.js" type="text/javascript" language="javascript"></script>
+    <script src="lib/ia3.js" type="text/javascript" language="javascript"></script>
+    <script src="js/site.js" type="text/javascript" language="javascript"></script>
 
-	</head>
-	<body>
-		<div id="page">
-			<div id="menu" class="wrapper">
-				<div>
-					<a href="/" title="Image A Trois">Image A Trois</a>
-					<ul>
-						<li><a href="#_try">Try it yourself</a></li>
-						<li><a href="#_how">How it works</a></li>
-						<li><a href="#_install">Install it, NOW!</a></li>
-						<li><a href="#_contact">Contact</a></li>
-					</ul>
-				</div>
-			</div>
-			<div id="header" class="wrapper row">
-				<div>
-					<div>
-						<h1>Hello designers</h1>
-						<p>It is time to end the discussion and start blaming the developer,<br />
-							because now you can easilly point out he did a bad job.</p>
-						<p>Image-A-Trois helps you find the differences between<br />
-							a design and the actuall webpage.</p>
-					</div>
-				</div>
-				<div>
-					<div id="header-website">
-						<img src="img/header-website.png" border="0" alt="compare website" />
-						<img src="img/header-shadow.png" class="shadow" border="0" alt="image-a-trois" />
-					</div>
-				</div>
-			</div>
-			<div class="wrapper">
-				<div id="header-loop">
-					<img src="img/header-loop.png" border="0" alt="find differences" />
-				</div>
-			</div>
-			<div id="_try" class="wrapper row">
-				<div>
-					<div>
-						<h2>Try it yourself</h2>
-						<p>The Image-A-Trois overlay on the right has been set up to contain the graphic design of this very page (with alternate colors for a better perception).</p>
-						<p>Try dragging and resizing the overlay, zoom in, scroll or resize your browser. Image-A-Trois-doesn't care.</p>
-						<p>Bored of just dragging and resizing?<br />
-							Try the advanced options as explained below.</p>
-					</div>
-				</div>
-				<div>
-					<div id="example"></div>
-				</div>
-			</div>
-			<div id="_how" class="wrapper row">
-				<div>
-					<div>
-						<h4>Advanced options</h4>
-						<ol>
-							<li>Hover over the overlay and click the pin-icon to pin it to the page.</li>
-							<li>Click and drag the bottom part of the overlay to set its opacity.</li>
-						</ol>
-					</div>
-				</div>
-				<div>
-					<div>
-						<h2>How it works</h2>
-						<p>Image-A-Trois allows you to compare a webpage agains up to three images (probably an image of the graphic design), just by clicking a bookmarklet.</p>
-						<p>Why three images? Well, at first it could handle only one image, but &quot;Image-A-Un&quot; just didn't sound that catchy.<br />
-							It is limited to three for that same reason.</p>
-						<h4>Test your developer in only four steps</h4>
-						<ul>
-							<li>Install the bookmarklet, visit a webpage and click it;</li>
-							<li>Specify how the page is aligned (left or centered);</li>
-							<li>Select the graphic design (any image will do);</li>
-							<li>Drag, scroll and find the differences.</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div id="_install" class="wrapper row">
-				<div>
-					<div>
-						<h2>Install it, NOW!</h2>
-						<p>Installing Image-A-Trois couldn't be easier,<br />
-							just drag this button to the bookmarks bar.<br />
-							Done.</p>
-						<a href="#" id="bookmarklet" title="Image A Trois">Image A Trois</a>
-					</div>
-				</div>
-				<div id="tape"></div>
-			</div>
-			<div id="_contact" class="wrapper">
-				<div>
-					<h2>Contact</h2>
-					<div class="lovefloat"><img src="img/dave-photo.png" border="0" alt="Dave Meister" />
-					<p>Hi, I'm Dave. I'm maintaining the Image-A-Trois JavaScript library and <a href="http://drupal.org/project/image_a_trois" target="_blank">Drupal module</a> these days.<br /> If you have suggestions or issues, please let me know by filing an issue on <a href="https://github.com/thedavidmeister/jquery.ia3" target="_blank">the Github project.</a></p></div>
-					<div class="lovefloat"><img src="img/contact-photo.png" border="0" alt="Fred Heusschen" />
-					<p>This is Fred, he created Image-A-Trois.<br />
-						Please visit <a href="http://www.frebsite.nl/en.php" title="FrebSite" target="_blank">his website</a> for some of his other projects.</p>
-					<p>Image-A-Trois was built using the <a href="http://www.jquery.com" target="_blank" title="jQuery">jQuery-library</a>, it is dual licensed under the <a href="http://en.wikipedia.org/wiki/MIT_License" target="_blank">MIT</a> and <a href="http://en.wikipedia.org/wiki/GNU_General_Public_License" target="_blank">GPL</a> licenses.</p>
-					</div>
-					<!-- AddThis Button BEGIN -->
-					<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-						<a class="addthis_button_preferred_1"></a>
-						<a class="addthis_button_preferred_2"></a>
-						<a class="addthis_button_preferred_3"></a>
-						<a class="addthis_button_preferred_4"></a>
-						<a class="addthis_button_compact"></a>
-					</div>
-					<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4f5e3fef6063e9c4"></script>
-					<!-- AddThis Button END -->
-				</div>
-			</div>
-		</div>
+  </head>
+  <body>
+    <div id="page">
+      <div id="menu" class="wrapper">
+        <div>
+          <a href="/" title="Image A Trois">Image A Trois</a>
+          <ul>
+            <li><a href="#_try">Try it yourself</a></li>
+            <li><a href="#_how">How it works</a></li>
+            <li><a href="#_install">Install it, NOW!</a></li>
+            <li><a href="#_contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="header" class="wrapper row">
+        <div>
+          <div>
+            <h1>Hello designers</h1>
+            <p>It is time to end the discussion and start blaming the developer,<br />
+              because now you can easilly point out he did a bad job.</p>
+            <p>Image-A-Trois helps you find the differences between<br />
+              a design and the actuall webpage.</p>
+          </div>
+        </div>
+        <div>
+          <div id="header-website">
+            <img src="img/header-website.png" border="0" alt="compare website" />
+            <img src="img/header-shadow.png" class="shadow" border="0" alt="image-a-trois" />
+          </div>
+        </div>
+      </div>
+      <div class="wrapper">
+        <div id="header-loop">
+          <img src="img/header-loop.png" border="0" alt="find differences" />
+        </div>
+      </div>
+      <div id="_try" class="wrapper row">
+        <div>
+          <div>
+            <h2>Try it yourself</h2>
+            <p>The Image-A-Trois overlay on the right has been set up to contain the graphic design of this very page (with alternate colors for a better perception).</p>
+            <p>Try dragging and resizing the overlay, zoom in, scroll or resize your browser. Image-A-Trois-doesn't care.</p>
+            <p>Bored of just dragging and resizing?<br />
+              Try the advanced options as explained below.</p>
+          </div>
+        </div>
+        <div>
+          <div id="example"></div>
+        </div>
+      </div>
+      <div id="_how" class="wrapper row">
+        <div>
+          <div>
+            <h4>Advanced options</h4>
+            <ol>
+              <li>Hover over the overlay and click the pin-icon to pin it to the page.</li>
+              <li>Click and drag the bottom part of the overlay to set its opacity.</li>
+            </ol>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2>How it works</h2>
+            <p>Image-A-Trois allows you to compare a webpage agains up to three images (probably an image of the graphic design), just by clicking a bookmarklet.</p>
+            <p>Why three images? Well, at first it could handle only one image, but &quot;Image-A-Un&quot; just didn't sound that catchy.<br />
+              It is limited to three for that same reason.</p>
+            <h4>Test your developer in only four steps</h4>
+            <ul>
+              <li>Install the bookmarklet, visit a webpage and click it;</li>
+              <li>Specify how the page is aligned (left or centered);</li>
+              <li>Select the graphic design (any image will do);</li>
+              <li>Drag, scroll and find the differences.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div id="_install" class="wrapper row">
+        <div>
+          <div>
+            <h2>Install it, NOW!</h2>
+            <p>Installing Image-A-Trois couldn't be easier,<br />
+              just drag this button to the bookmarks bar.<br />
+              Done.</p>
+            <a href="#" id="bookmarklet" title="Image A Trois">Image A Trois</a>
+          </div>
+        </div>
+        <div id="tape"></div>
+      </div>
+      <div id="_contact" class="wrapper">
+        <div>
+          <h2>Contact</h2>
+          <div class="lovefloat"><img src="img/dave-photo.png" border="0" alt="Dave Meister" />
+          <p>Hi, I'm Dave. I'm maintaining the Image-A-Trois JavaScript library and <a href="http://drupal.org/project/image_a_trois" target="_blank">Drupal module</a> these days.<br /> If you have suggestions or issues, please let me know by filing an issue on <a href="https://github.com/thedavidmeister/jquery.ia3" target="_blank">the Github project.</a></p></div>
+          <div class="lovefloat"><img src="img/contact-photo.png" border="0" alt="Fred Heusschen" />
+          <p>This is Fred, he created Image-A-Trois.<br />
+            Please visit <a href="http://www.frebsite.nl/en.php" title="FrebSite" target="_blank">his website</a> for some of his other projects.</p>
+          <p>Image-A-Trois was built using the <a href="http://www.jquery.com" target="_blank" title="jQuery">jQuery-library</a>, it is dual licensed under the <a href="http://en.wikipedia.org/wiki/MIT_License" target="_blank">MIT</a> and <a href="http://en.wikipedia.org/wiki/GNU_General_Public_License" target="_blank">GPL</a> licenses.</p>
+          </div>
+          <!-- AddThis Button BEGIN -->
+          <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+            <a class="addthis_button_preferred_1"></a>
+            <a class="addthis_button_preferred_2"></a>
+            <a class="addthis_button_preferred_3"></a>
+            <a class="addthis_button_preferred_4"></a>
+            <a class="addthis_button_compact"></a>
+          </div>
+          <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4f5e3fef6063e9c4"></script>
+          <!-- AddThis Button END -->
+        </div>
+      </div>
+    </div>
 
-	</body>
+  </body>
 </html>
